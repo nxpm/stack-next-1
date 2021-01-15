4969148c527d1855198558477a4918fd9398e0ec
Bram Borggreve <borggreve@gmail.com>
2021-01-15 15:17:05 -0500

    feat: implement web plugin

---
+++ e2e/web-e2e/tests/web.spec.ts
@@ -0,0 +1,31 @@
+import { checkFilesExist, ensureNxProject, readJson, runNxCommandAsync, uniq } from '@nrwl/nx-plugin/testing'
+describe('web e2e', () => {
+  it('should create web', async (done) => {
+    const plugin = uniq('web')
+    ensureNxProject('@nxpm/web', 'dist/packages/web')
+    await runNxCommandAsync(`generate @nxpm/web:application ${plugin}`)
+
+    done()
+  })
+
+  describe('--directory', () => {
+    xit('should create src in the specified directory', async (done) => {
+      const plugin = uniq('web')
+      ensureNxProject('@nxpm/web', 'dist/packages/web')
+      await runNxCommandAsync(`generate @nxpm/web:web ${plugin} --directory subdir`)
+      expect(() => checkFilesExist(`libs/subdir/${plugin}/src/index.ts`)).not.toThrow()
+      done()
+    })
+  })
+
+  describe('--tags', () => {
+    xit('should add tags to nx.json', async (done) => {
+      const plugin = uniq('web')
+      ensureNxProject('@nxpm/web', 'dist/packages/web')
+      await runNxCommandAsync(`generate @nxpm/web:web ${plugin} --tags e2etag,e2ePackage`)
+      const nxJson = readJson('nx.json')
+      expect(nxJson.projects[plugin].tags).toEqual(['e2etag', 'e2ePackage'])
+      done()
+    })
+  })
+})
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/packages/api', '<rootDir>/e2e/api-e2e'],
+  projects: ['<rootDir>/packages/api', '<rootDir>/e2e/api-e2e', '<rootDir>/packages/web', '<rootDir>/e2e/web-e2e'],
 }
